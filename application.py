7275ba7b434e46a83041c69a3bccee0075079b36
intel <akhil19942@gamil.com>
2022-07-23 12:22:32 +0530

    init

---
+++ application.py
@@ -0,0 +1,22 @@
+#importing libraries
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+application=Flask(__name__)
+app=application
+
+
+@app.route('/',methods=['GET'])
+def home():
+    df=pd.read_json('https://www.mohfw.gov.in/data/datanew.json')
+    data=[]
+    for i in df.iloc[:36].index:
+        data.append([df['sno'][i],df['state_name'][i],df['active'][i],df['cured'][i],df['positive'][i],df['death'][i]])
+    return render_template('index.html',df=data)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
